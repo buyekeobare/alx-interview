15a2d5ef5d84178e7a0812404bb29eb170b12698
buyekeobare <buyekeobare@gmail.com>
2024-10-03 11:35:22 -0700

    file updated

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,13 +4,13 @@
 
 def pascal_triangle(n):
     """Triangle"""
-    if r <= 0:
+    if n <= 0:
         return []
     pas = [[1]]
-    for row_number in range(1, r):
+    for row_number in range(1, n):
         row = [1]
-        for i in range(1, row_number):
-            element = pas[row_number - 1][i - 1] + pas[row_number - 1][i]
+        for j in range(1, row_number):
+            element = pas[row_number - 1][j - 1] + pas[row_number - 1][j]
             row.append(element)
         row.append(1)
         pas.append(row)
